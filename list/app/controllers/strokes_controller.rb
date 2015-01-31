3ba3131c1659d093a6748276c3d0992206ae059b
arcoyk <sanhai.sato@gmail.com>
2015-02-01 00:51:36 +0900

    easy suggest from html

--- list/app/controllers/strokes_controller.rb
+++ list/app/controllers/strokes_controller.rb
@@ -1,3 +1,7 @@
+require "uri"
+require "open-uri"
+require "nokogiri"
+
 class StrokesController < ApplicationController
   def index
   	@strokes = Stroke.all
@@ -11,11 +15,16 @@ class StrokesController < ApplicationController
   end
 
   def create
+    http_schema = /^(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/
   	@stroke = Stroke.new
   	@stroke.tags = params[:stroke][:tags]
   	@stroke.content = params[:stroke][:content]
-    @stroke.like = 0
     @stroke.icon = "default.png"
+    @stroke.like = 0
+    if @stroke.content.match http_schema
+      @stroke.like = 100
+      @stroke = self.auto_digest @stroke.content
+    end
   	@stroke.save
   	redirect_to '/strokes/index'
   end
@@ -45,5 +54,52 @@ class StrokesController < ApplicationController
     redirect_to '/strokes/index'
   end
 
+  def auto_tag str
+    words = str.split(" ")
+    words.sort! do |w1, w2|
+      words.count(w1) <=> words.count(w2)
+    end
+    words.uniq!
+    return words[0, 3]
+  end
+
+  def auto_digest url
+    doc = Nokogiri::HTML open(url)
+    title = doc.css("title")[0].children[0].text
+    img_nodeset = doc.css("img")
+    p_nodeset = doc.css("p")
+    imgs = Array.new
+    img_nodeset.each do |node|
+      imgs.push node
+    end
+    imgs.sort! do |img1, img2|
+      width1 = img1.attribute("width")
+      width2 = img2.attribute("width")
+      value1 = 0
+      value2 = 0
+      if width1 != nil
+        value1 = width1.value.to_i
+      end
+      if width2 != nil
+        value2 = width2.value.to_i
+      end
+      value1 <=> value2
+    end
+    text = ""
+    p_nodeset.each do |node|
+      node.children.each do |child|
+        text += child.text
+      end
+    end
+    tags = self.auto_tag(text)
+    img_href = imgs.first
+    stroke = Stroke.new
+    stroke.content = title
+    stroke.icon = imgs.first.attribute("src").value
+    stroke.tags = tags.join(",")
+    stroke.like = 0
+    stroke.url = url
+    return stroke
+  end
 
 end
