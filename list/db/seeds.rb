acf9d63e121aecf3ea2ae15d8c355970b09e5b3b
arcoyk <sanhai.sato@gmail.com>
2015-02-11 04:07:29 +0900

    backroll

--- list/db/seeds.rb
+++ list/db/seeds.rb
@@ -6,27 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-@user = User.new
-@user.name = 'Yui Kita'
-@user.username = 'Meaue'
-@user.location = 'Tokyo, Japan'
-@user.about = 'Yui is a hacker'
-@user.save
-
-@user = User.new
-@user.name = 'Keisuke Igrashi'
-@user.username = 'Rupin'
-@user.location = 'Tokyo, Japan'
-@user.about = 'Keisuke is a cook'
-@user.save
-
-@stroke = Stroke.new
-@stroke.tags = "book, @meaue, romance"
-@stroke.content = "Dreamers (1988) Albert Adair is a romance novel"
-@stroke.icon = "default.png"
-@stroke.url = "http://google.com"
-@stroke.like = 344
-@stroke.save
 
 @stroke = Stroke.new
 @stroke.tags = "movie, @meaue, SF"
